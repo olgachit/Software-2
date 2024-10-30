247e9436b5cd2819e7bb43d348337c34c2710425
olgachit <olga.chitembo@metropolia.fi>
2024-10-30 15:36:16 +0200

    push

--- Assignments/Inheritance.py
+++ Assignments/Inheritance.py
@@ -8,29 +8,32 @@ class Person:
 
 class Member(Person):
     def __init__(self, name, age, gender, membership_id):
-        super().__init__(name, age, gender)
+        Person.__init__(name, age, gender)
         self.membership_id = membership_id
     def introduce(self):
-        return f"{super().introduce()} with membership ID: {self.membership}"
+        return f"{super().introduce()} with membership ID: {self.membership_id}"
 
 class Author(Person):
     def __init__(self, name, age, gender, books_written):
-        super().__init__(name,age,gender)
+        Person.__init__(name,age,gender)
         self.books_written = books_written
     def introduce(self):
-        return f"Books written: {self.books_written}"
+        return f"{super().introduce()}, Books written: {self.books_written}"
 
 class AuthorMember(Member, Author):
     def __init__(self, name, age, gender, membership_id, books_written):
-        Person.__init__(name, age, gender)
+        Person.__init__(self,name, age, gender)
         self.membership_id = membership_id
         self.books_written = books_written
     def introduce(self):
-        return f"I am {self.name}, {self.age} years old, {self.gender} with memberhip ID: {self.member}, Books written: {self.books_written}"
+        return f"{super().introduce()}"
 
 library_members= [
     AuthorMember("Mary", 20, "female", "M001", "abc"),
     AuthorMember("John", 21, "male", "M002", "cdb")
 ]
+
 for member in library_members:
     print(member.introduce())
+#author1= AuthorMember("Mary", 20, "female", "M001", "abc")
+#print(author1.introduce())
