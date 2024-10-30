490575a7eaf8dd258c023e12e5674681ff0fd310
olgachit <olga.chitembo@metropolia.fi>
2024-10-30 17:30:07 +0200

    push

--- Exercises/module10.py
+++ Exercises/module10.py
@@ -1,5 +1,4 @@
 # 1 Write an Elevator class that receives the numbers of the bottom and top floors as initializer parameters. The elevator has methods go_to_floor, floor_up and floor_down. A new elevator is always at the bottom floor. If you make elevator h for example the method call h.go_to_floor(5), the method calls either the floor_up or floor_down methods as many times as it needs to get to the fifth floor. The methods run the elevator one floor up or down and tell what floor the elevator is after each move. Test the class by creating an elevator in the main program, tell it to move to a floor of your choice and then back to the bottom floor.
-'''
 class Elevator:
     current_floor = 0
     def __init__(self, bottom_floor, top_floor):
@@ -27,7 +26,7 @@ class Elevator:
 h=Elevator(1,5)
 h.go_to_floor(5)
 h.go_to_floor(1)
-'''
+
 # 2 Extend the previous program by creating a Building class. The initializer parameters for the class are the numbers of the bottom and top floors and the number of elevators in the building. When a building is created, the building creates the required number of elevators. The list of elevators is stored as a property of the building. Write a method called run_elevator that accepts the number of the elevator and the destination floor as its parameters. In the main program, write the statements for creating a new building and running the elevators of the building.
 class Elevator:
     current_floor = 0
